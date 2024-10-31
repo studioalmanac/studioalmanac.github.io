0f81b376482cb8764f034d3d5d697e9772a71e7b
Parker Roth <154391879+yawnybear@users.noreply.github.com>
2024-10-31 09:16:08 -0400

    change js

--- scripts/sa-scripts.js
+++ scripts/sa-scripts.js
@@ -1,14 +1,29 @@
 // sa-script.js
-let slideIndex = 1;
-showSlides(slideIndex);function plusSlide(n) {
-  showSlides(slideIndex += n);
-}function showSlides(n) {
-  let i;
-  const slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+// document.addEventListener('DOMContentLoaded', function() {
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  console.log("hidad")
+
+  // Auto rotate every 5 seconds
+  setInterval(function() {
+    console.log("himom")
+      plusSlide(1);
+  }, 5000);
+
+  // Making plusSlide available globally
+  window.plusSlide = function(n) {
+      showSlides(slideIndex += n);
   }
-  slides[slideIndex-1].style.display = "block";
-}
+
+  function showSlides(n) {
+      let i;
+      const slides = document.getElementsByClassName("slide");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      slides[slideIndex-1].style.display = "block";
+  }
+// });
